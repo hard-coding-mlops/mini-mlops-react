d29ef0c79b7376743c8eafa2e1f121b755487835
Monte <monte6198@gmail.com>
2023-11-13 14:48:46 +0900

    feat: SideBar 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import SideBar from './components/SideBar/SideBar';
+import History from './pages/History';
+
 function App() {
   return (
-    <div>
-      <p>mini mlops</p>
-      <p>by. team hard-coding</p>
+    <div className='App'>
+      <SideBar />
+      {/* <hr /> */}
+      <Routes>
+        <Route path='/' element={<div>Pipeline</div>} />
+        <Route path='/history' element={<History />} />
+        <Route path='/service' element={<div>Service</div>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,38 @@
+import { useNavigate, useLocation } from 'react-router-dom';
+import styles from './SideBar.module.css';
+
+function SideBar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  return (
+    <div className={styles.sideBar}>
+      <button
+        className={`${styles.navigationButton} ${location.pathname === '/history' ? styles.activeButton : ''}`}
+        onClick={() => {
+          navigate('/history');
+        }}
+      >
+        HISTORY
+      </button>
+      <button
+        className={`${styles.navigationButton} ${location.pathname === '/' ? styles.activeButton : ''}`}
+        onClick={() => {
+          navigate('/');
+        }}
+      >
+        PIPELINE
+      </button>
+      <button
+        className={`${styles.navigationButton} ${location.pathname === '/service' ? styles.activeButton : ''}`}
+        onClick={() => {
+          navigate('/service');
+        }}
+      >
+        SERVICE
+      </button>
+    </div>
+  );
+}
+
+export default SideBar;
