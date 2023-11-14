9afcda761cba8fd1cddf32e811d4c8267c143f5b
Monte <monte6198@gmail.com>
2023-11-14 15:48:39 +0900

    feat: Pipeline 페이지 타이머 추가

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import SideBar from './components/SideBar/SideBar';
 import History from './pages/History/History';
 import Service from './pages/Service/Service';
+import Pipeline from './pages/Pipeline/Pipeline';
 
 function App() {
   return (
     <div className='App'>
       <SideBar />
-      {/* <hr /> */}
       <Routes>
-        <Route path='/' element={<div>Pipeline</div>} />
+        <Route path='/' element={<Pipeline />} />
         <Route path='/history' element={<History />} />
         <Route path='/history/:recordId' element={<History />} />
         <Route path='/service' element={<Service />} />
---
+++ src/pages/Pipeline/Pipeline.jsx
@@ -0,0 +1,14 @@
+import PageHeader from '../../components/PageHeader/PageHeader';
+
+import styles from './Pipeline.module.css';
+import PipelineHeader from './PipelineHeader';
+
+function Pipeline() {
+  return (
+    <>
+      <PipelineHeader />
+    </>
+  );
+}
+
+export default Pipeline;
