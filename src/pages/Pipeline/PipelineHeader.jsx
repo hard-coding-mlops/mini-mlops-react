39187c588e16da638e61907a0cad78ca364bfa40
Monte <monte6198@gmail.com>
2023-11-14 15:49:40 +0900

    fix: 새로고침 시 타이머 초기화 문제 해결

--- src/pages/Pipeline/PipelineHeader.jsx
+++ src/pages/Pipeline/PipelineHeader.jsx
@@ -2,33 +2,36 @@ import React, { useEffect, useState } from 'react';
 import PageHeader from '../../components/PageHeader/PageHeader';
 import styles from './Pipeline.module.css';
 
+function calculateTimeLeft() {
+  // 서울 기준 다음 날 오전 9시 30분
+  const targetTime = new Date();
+  targetTime.setDate(targetTime.getDate() + 1);
+  targetTime.setHours(9, 30, 0, 0);
+
+  // 현재 시간
+  const currentTime = new Date();
+
+  // 남은 시간 계산
+  const timeDifference = targetTime - currentTime;
+
+  // 시, 분, 초 계산
+  const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  // 시간 형식으로 포맷팅
+  return `${String(hours).padStart(2, '0')}시 ${String(minutes).padStart(2, '0')}분 ${String(seconds).padStart(
+    2,
+    '0',
+  )}초`;
+}
+
 function PipelineHeader() {
-  const [timeLeft, setTimeLeft] = useState('00시 00분 00초');
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      // 서울 기준 다음 날 오전 9시 30분
-      const targetTime = new Date();
-      targetTime.setDate(targetTime.getDate() + 1);
-      targetTime.setHours(9, 30, 0, 0);
-
-      // 현재 시간
-      const currentTime = new Date();
-
-      // 남은 시간 계산
-      const timeDifference = targetTime - currentTime;
-
-      // 시, 분, 초 계산
-      const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-
-      // 시간 형식으로 포맷팅
-      const formattedTimeLeft = `${String(hours).padStart(2, '0')}시 ${String(minutes).padStart(2, '0')}분 ${String(
-        seconds,
-      ).padStart(2, '0')}초`;
-
-      setTimeLeft(formattedTimeLeft);
+      setTimeLeft(calculateTimeLeft);
     }, 1000); // 1초마다 업데이트
 
     // 컴포넌트 언마운트 시 clearInterval을 통해 interval 정리
