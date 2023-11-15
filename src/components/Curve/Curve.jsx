a929487953febfa774cb4f27602693128bec1673
Monte <monte6198@gmail.com>
2023-11-15 17:12:16 +0900

    feat: Progress 진행 애니메이션 추가

--- src/components/Curve/Curve.jsx
+++ src/components/Curve/Curve.jsx
@@ -1,19 +1,24 @@
-function Curve({ colorHexCode, startX, startY, endX, endY }) {
-  const startPoint = { x: startX, y: startY };
-  const endPoint = { x: endX, y: endY };
-  const controlPoint = { x: endX, y: startY };
+import styles from './Curve.module.css';
 
-  const pathData = `M${startPoint.x},${startPoint.y} Q${controlPoint.x},${controlPoint.y} ${endPoint.x},${endPoint.y}`;
+function Curve({ colorHexCode, startX, startY, endX, endY, isFinished = false }) {
+  const middlePoint = { x: (endX + startX) / 2, y: (endY + startY) / 2 };
+  const firstControlPoint = { x: middlePoint.x, y: startY };
+  const secondControlPoint = { x: middlePoint.x, y: endY };
+
+  const pathData = `M ${startX} ${startY} C ${startX} ${startY}, ${firstControlPoint.x} ${firstControlPoint.y}, ${middlePoint.x} ${middlePoint.y} C ${middlePoint.x} ${middlePoint.y}, ${secondControlPoint.x} ${secondControlPoint.y}, ${endX} ${endY}`;
 
   return (
     <svg width='1960' height='1080' style={{ position: 'absolute', zIndex: -1, top: '15px', left: '15px' }}>
-      {/* 점 */}
-      <circle cx={startPoint.x} cy={startPoint.y} r='0' fill='black' />
-      <circle cx={endPoint.x} cy={endPoint.y} r='0' fill='black' />
-
-      {/* 선 */}
-      <path d={pathData} fill='none' stroke={colorHexCode} strokeWidth='2px' />
+      <path d={pathData} fill='none' stroke='#cccccc' strokeWidth='2px' />
+      <path
+        className={isFinished && styles.colorLine}
+        d={pathData}
+        fill='none'
+        stroke={isFinished ? colorHexCode : '#cccccc'}
+        strokeWidth='2px'
+      />
     </svg>
   );
 }
+
 export default Curve;
--- src/pages/Pipeline/Pipeline.jsx
+++ src/pages/Pipeline/Pipeline.jsx
@@ -1,27 +1,59 @@
+import { useState } from 'react';
 import Curve from '../../components/Curve/Curve';
 import ProgressCircle from '../../components/ProgressCircle/ProgressCircle';
 import styles from './Pipeline.module.css';
 import PipelineHeader from './PipelineHeader';
 
 function Pipeline() {
+  const [scrapingProgress, setScrapingProgress] = useState(false);
+  const [preprocessingProgress, setPreprocessingProgress] = useState(false);
+  const [learningProgress, setLearningProgress] = useState(false);
+
   return (
     <div className={styles.pipeline}>
       <PipelineHeader />
       <div className={styles.timeline}>
         <ProgressCircle colorHexCode='#ff6b6b' coordinate={{ x: 480, y: 300 }} label='데이터 수집' labelPosition='d' />
-
-        <Curve startX={480} startY={300} endX={696.5} endY={575} colorHexCode='#ff6b6b' />
-        <Curve startX={913} startY={850} endX={696.5} endY={575} colorHexCode='#ff6b6b' />
+        <button
+          onClick={() => {
+            setScrapingProgress(true);
+          }}
+        >
+          STEP 1
+        </button>
+        <Curve startX={480} startY={300} endX={913} endY={850} colorHexCode='#ff6b6b' isFinished={scrapingProgress} />
+        {/* <Curve startX={480} startY={300} endX={913} endY={850} colorHexCode='#cccccc' /> */}
 
         <ProgressCircle colorHexCode='#ffa500' coordinate={{ x: 913, y: 850 }} label='데이터 정제' labelPosition='u' />
+        <button
+          onClick={() => {
+            setPreprocessingProgress(true);
+          }}
+        >
+          STEP 2
+        </button>
 
-        <Curve startX={913} startY={850} endX={1129.5} endY={575} colorHexCode='#ffa500' />
-        <Curve startX={1347} startY={300} endX={1129.5} endY={575} colorHexCode='#ffa500' />
+        <Curve
+          startX={913}
+          startY={850}
+          endX={1347}
+          endY={300}
+          colorHexCode='#ffa500'
+          isFinished={preprocessingProgress}
+        />
+        {/* <Curve startX={913} startY={850} endX={1347} endY={300} colorHexCode='#cccccc' /> */}
 
         <ProgressCircle colorHexCode='#3498db' coordinate={{ x: 1347, y: 300 }} label='모델 학습' labelPosition='d' />
+        <button
+          onClick={() => {
+            setLearningProgress(true);
+          }}
+        >
+          STEP 3
+        </button>
 
-        <Curve startX={1347} startY={300} endX={1563.5} endY={575} colorHexCode='#3498db' />
-        <Curve startX={1780} startY={850} endX={1563.5} endY={575} colorHexCode='#3498db' />
+        <Curve startX={1347} startY={300} endX={1780} endY={850} colorHexCode='#3498db' isFinished={learningProgress} />
+        {/* <Curve startX={1347} startY={300} endX={1780} endY={850} colorHexCode='#cccccc' /> */}
 
         <ProgressCircle colorHexCode='#4caf50' coordinate={{ x: 1780, y: 850 }} label='모델 배포' labelPosition='u' />
       </div>
