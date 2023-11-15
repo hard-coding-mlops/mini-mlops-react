232f93a7dce1e94d425adc09d10c8d27f88a24ef
Monte <monte6198@gmail.com>
2023-11-15 17:13:20 +0900

    fix: 애니메이션 경고 해결

--- src/components/Curve/Curve.jsx
+++ src/components/Curve/Curve.jsx
@@ -11,7 +11,7 @@ function Curve({ colorHexCode, startX, startY, endX, endY, isFinished = false })
     <svg width='1960' height='1080' style={{ position: 'absolute', zIndex: -1, top: '15px', left: '15px' }}>
       <path d={pathData} fill='none' stroke='#cccccc' strokeWidth='2px' />
       <path
-        className={isFinished && styles.colorLine}
+        className={isFinished ? styles.colorLine : ''}
         d={pathData}
         fill='none'
         stroke={isFinished ? colorHexCode : '#cccccc'}
