b0d17896cd1558ad0e6429bb840ab2cfd972633e
Monte <monte6198@gmail.com>
2023-11-15 14:27:25 +0900

    feat: Progress Label 추가

--- src/components/ProgressCircle/ProgressCircle.jsx
+++ src/components/ProgressCircle/ProgressCircle.jsx
@@ -1,11 +1,22 @@
 import styles from './ProgressCircle.module.css';
 
-function ProgressCircle({ colorHexCode, coordinate }) {
+function ProgressCircle({ colorHexCode, coordinate, label, labelPosition }) {
   // coordinate가 정의되지 않았거나 x, y 속성이 없는 경우를 방지하기 위해 기본값을 설정합니다.
   const { x = 0, y = 0 } = coordinate || {};
 
+  const labelStyle = {
+    color: colorHexCode,
+    left: `${x - 85}px`,
+    top: `${labelPosition === 'u' && labelPosition !== 'd' ? y - 50 : y + 50}px`,
+  };
+
   return (
-    <div className={styles.progress} style={{ backgroundColor: colorHexCode, left: `${x}px`, top: `${y}px` }}></div>
+    <>
+      <div className={styles.progress} style={{ backgroundColor: colorHexCode, left: `${x}px`, top: `${y}px` }}></div>
+      <span className={styles.progressLabel} style={{ ...labelStyle }}>
+        {label}
+      </span>
+    </>
   );
 }
 
--- src/pages/Pipeline/Pipeline.jsx
+++ src/pages/Pipeline/Pipeline.jsx
@@ -8,23 +8,22 @@ function Pipeline() {
     <div className={styles.pipeline}>
       <PipelineHeader />
       <div className={styles.timeline}>
-        {/* 
-          너비 1660px 화면에 점 4개를 균등하게 배치하려면 각 점의 x좌표는 553, 1106, 1659가 됩니다.
-        */}
-        <ProgressCircle colorHexCode='#ff6b6b' coordinate={{ x: 480, y: 300 }} />
-        <span>데이터 수집</span>
+        <ProgressCircle colorHexCode='#ff6b6b' coordinate={{ x: 480, y: 300 }} label='데이터 수집' labelPosition='d' />
+
         <Curve startX={480} startY={300} endX={696.5} endY={575} colorHexCode='#ff6b6b' />
         <Curve startX={913} startY={850} endX={696.5} endY={575} colorHexCode='#ff6b6b' />
-        <ProgressCircle colorHexCode='#ffa500' coordinate={{ x: 913, y: 850 }} />
-        <span>데이터 정제</span>
+
+        <ProgressCircle colorHexCode='#ffa500' coordinate={{ x: 913, y: 850 }} label='데이터 정제' labelPosition='u' />
+
         <Curve startX={913} startY={850} endX={1129.5} endY={575} colorHexCode='#ffa500' />
         <Curve startX={1347} startY={300} endX={1129.5} endY={575} colorHexCode='#ffa500' />
-        <ProgressCircle colorHexCode='#3498db' coordinate={{ x: 1347, y: 300 }} />
-        <span>모델 학습</span>
+
+        <ProgressCircle colorHexCode='#3498db' coordinate={{ x: 1347, y: 300 }} label='모델 학습' labelPosition='d' />
+
         <Curve startX={1347} startY={300} endX={1563.5} endY={575} colorHexCode='#3498db' />
         <Curve startX={1780} startY={850} endX={1563.5} endY={575} colorHexCode='#3498db' />
-        <ProgressCircle colorHexCode='#4caf50' coordinate={{ x: 1780, y: 850 }} />
-        <span>모델 배포</span>
+
+        <ProgressCircle colorHexCode='#4caf50' coordinate={{ x: 1780, y: 850 }} label='모델 배포' labelPosition='u' />
       </div>
     </div>
   );
