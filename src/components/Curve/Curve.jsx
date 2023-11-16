03610d0870a2540bf87f1711680780f8f0989d7e
Monte <monte6198@gmail.com>
2023-11-16 18:08:11 +0900

    feat: 곡선 스타일 수정

--- src/components/Curve/Curve.jsx
+++ src/components/Curve/Curve.jsx
@@ -1,11 +1,7 @@
 import styles from './Curve.module.css';
 
 function Curve({ colorHexCode, startX, startY, endX, endY, isFinished = false }) {
-  const middlePoint = { x: (endX + startX) / 2, y: (endY + startY) / 2 };
-  const firstControlPoint = { x: middlePoint.x, y: startY };
-  const secondControlPoint = { x: middlePoint.x, y: endY };
-
-  const pathData = `M ${startX} ${startY} C ${startX} ${startY}, ${firstControlPoint.x} ${firstControlPoint.y}, ${middlePoint.x} ${middlePoint.y} C ${middlePoint.x} ${middlePoint.y}, ${secondControlPoint.x} ${secondControlPoint.y}, ${endX} ${endY}`;
+  const pathData = `M ${startX}, ${startY} C ${endX}, ${startY} ${startX}, ${endY} ${endX}, ${endY}`;
 
   return (
     <svg width='1960' height='1080' style={{ position: 'absolute', zIndex: -1, top: '15px', left: '15px' }}>
