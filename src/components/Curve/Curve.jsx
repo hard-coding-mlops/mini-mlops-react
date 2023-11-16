2f3001b84e720704ca83602e7bfa81376ff6e35b
Monte <monte6198@gmail.com>
2023-11-16 18:10:59 +0900

    feat: 곡선 path length 수정

--- src/components/Curve/Curve.jsx
+++ src/components/Curve/Curve.jsx
@@ -1,13 +1,22 @@
+import { useEffect, useRef } from 'react';
 import styles from './Curve.module.css';
 
 function Curve({ colorHexCode, startX, startY, endX, endY, isFinished = false }) {
   const pathData = `M ${startX}, ${startY} C ${endX}, ${startY} ${startX}, ${endY} ${endX}, ${endY}`;
+  const pathRef = useRef(null);
+
+  useEffect(() => {
+    const pathElement = pathRef.current;
+    const pathLength = pathElement.getTotalLength();
+    console.log('Path Length:', pathLength);
+  }, []); // Empty dependency array ensures that this effect runs only once after the initial render
 
   return (
     <svg width='1960' height='1080' style={{ position: 'absolute', zIndex: -1, top: '15px', left: '15px' }}>
-      <path d={pathData} fill='none' stroke='#cccccc' strokeWidth='2px' />
+      <path ref={pathRef} d={pathData} fill='none' stroke='#cccccc' strokeWidth='2px' />
       <path
         className={isFinished ? styles.colorLine : ''}
+        ref={pathRef}
         d={pathData}
         fill='none'
         stroke={isFinished ? colorHexCode : '#cccccc'}
