d2b1562e9b00d52d903b8226b7b36244fd13e076
taehwan01 <monte6198@gmail.com>
2023-12-06 16:03:43 +0900

    feat: 서비스 페이지 수정

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -6,39 +6,42 @@ import ROCKET_ICON from '../../assets/icons/rocket-icon.svg';
 import SAVE_ICON from '../../assets/icons/save-icon.svg';
 import TRASH_ICON from '../../assets/icons/trash-icon.svg';
 import USERLOG_ICON from '../../assets/icons/userlog-icon.svg';
+import SERVICE_ICON from '../../assets/icons/service-icon.svg';
 
 import styles from './Icon.module.css';
 
 const ICON_LABEL = {
-  add: ADD_ICON,
-  back: BACK_ICON,
-  csv: DOWNLOAD_ICON,
-  delete: TRASH_ICON,
-  edit: EDIT_ICON,
-  deploy: ROCKET_ICON,
-  save: SAVE_ICON,
-  userlog: USERLOG_ICON,
+    add: ADD_ICON,
+    back: BACK_ICON,
+    csv: DOWNLOAD_ICON,
+    delete: TRASH_ICON,
+    edit: EDIT_ICON,
+    deploy: ROCKET_ICON,
+    save: SAVE_ICON,
+    userlog: USERLOG_ICON,
+    service: SERVICE_ICON,
 };
 const LABEL_COLOR = {
-  add: '#3498DB',
-  back: '#FF6B6B',
-  csv: '#4CAF50',
-  delete: '#FF6B6B',
-  edit: '#FFA500',
-  deploy: '#FF6B6B',
-  save: '#FFA500',
-  userlog: '#3498DB',
+    add: '#3498DB',
+    back: '#FF6B6B',
+    csv: '#4CAF50',
+    delete: '#FF6B6B',
+    edit: '#FFA500',
+    deploy: '#FF6B6B',
+    save: '#FFA500',
+    userlog: '#3498DB',
+    service: '#3498DB',
 };
 
 function Icon({ label, handleOnClick }) {
-  return (
-    <button className={styles.iconWithLabel} onClick={handleOnClick}>
-      <img src={ICON_LABEL[label]} alt={label} className={styles.icon} />
-      <span className={styles.label} style={{ color: LABEL_COLOR[label] }}>
-        {label}
-      </span>
-    </button>
-  );
+    return (
+        <button className={styles.iconWithLabel} onClick={handleOnClick}>
+            <img src={ICON_LABEL[label]} alt={label} className={styles.icon} />
+            <span className={styles.label} style={{ color: LABEL_COLOR[label] }}>
+                {label.toUpperCase()}
+            </span>
+        </button>
+    );
 }
 
 export default Icon;
--- src/pages/NewsClassifier/NewsClassifier.jsx
+++ src/pages/NewsClassifier/NewsClassifier.jsx
@@ -3,71 +3,118 @@ import PageHeader from '../../components/PageHeader/PageHeader';
 import styles from './NewsClassifier.module.css';
 import axios from 'axios';
 import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
+import PageTemplate from '../PageTemplate/PageTemplate';
+import HeaderTemplate from '../PageTemplate/HeaderTemplate';
+import BodyTemplate from '../PageTemplate/BodyTemplate';
+import { useNavigate } from 'react-router-dom';
 
 function NewsClassifier() {
-  const articleInputRef = useRef(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isFocused, setIsFocused] = useState(false);
-  const [category, setCategory] = useState('');
+    const navigate = useNavigate();
+    const articleInputRef = useRef(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isFocused, setIsFocused] = useState(false);
+    const [category, setCategory] = useState('');
+    const [firstTry, setFirstTry] = useState(true);
 
-  const classifyArticle = async () => {
-    setIsLoading(true);
-    const article = articleInputRef.current.value;
+    const classifyArticle = async () => {
+        setIsLoading(true);
+        const article = articleInputRef.current.value;
 
-    // 3초 기다림(모델 예측 불러올 때에는 지울 것!)
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+        // 3초 기다림(모델 예측 불러올 때에는 지울 것!)
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+        // alert(article);
+        // const result = await axios.post(`${process.env.REACT_APP_SERVER_URL}/classifier/classify`, {
+        //   content: article,
+        // });
 
-    const result = await axios.post(`${process.env.REACT_APP_SERVER_URL}/classifier/classify`, {
-      content: article,
-    });
+        // setCategory(result.data.category);
 
-    setCategory(result.data.category);
+        setCategory('사회');
+        setIsLoading(false);
+        setFirstTry(false);
+    };
 
-    setIsLoading(false);
-  };
+    const resultOrGetResult = () => {
+        if (category !== '') {
+            return (
+                <PageHeader>
+                    <span>{category} 분야입니다.</span>
+                </PageHeader>
+            );
+        }
 
-  const resultOrGetResult = () => {
-    if (category !== '') {
-      return (
-        <PageHeader>
-          <span>{category} 분야입니다.</span>
-        </PageHeader>
-      );
-    }
+        return (
+            <>
+                {isLoading ? (
+                    <LoadingSpinner spinnerStyle={{ position: 'inherit' }} />
+                ) : (
+                    <PageHeader>
+                        <button className={styles.classifyingButton} onClick={classifyArticle}>
+                            <span>분류하기</span>
+                        </button>
+                    </PageHeader>
+                )}
+            </>
+        );
+    };
 
     return (
-      <>
-        {isLoading ? (
-          <LoadingSpinner spinnerStyle={{ position: 'inherit' }} />
-        ) : (
-          <PageHeader>
-            <button className={styles.classifyingButton} onClick={classifyArticle}>
-              <span>분류하기</span>
+        <div className={styles.newsClassifier}>
+            <button className={styles.toAdmin} onClick={() => navigate('/')}>
+                관리자이신가요?
             </button>
-          </PageHeader>
-        )}
-      </>
+            <span className={styles.modelName}>model_name_1 에게 물어보기</span>
+            <div className={styles.container}>
+                <textarea
+                    ref={articleInputRef}
+                    name='article-input'
+                    onFocus={() => setIsFocused(true)}
+                    onBlur={() => setIsFocused(false)}
+                    placeholder={isFocused ? '' : '기사를 입력해주세요.'}
+                    className={styles.articleInput}
+                    cols='100'
+                    rows='48'
+                />
+                <div className={styles.infoContainer}>
+                    <div className={styles.answer}>
+                        {category ? (
+                            <div>
+                                해당 기사는{' '}
+                                <span
+                                    style={{
+                                        fontSize: '2rem',
+                                        fontWeight: '800',
+                                        textDecoration: 'underline',
+                                    }}
+                                >
+                                    &nbsp;{category}&nbsp;
+                                </span>{' '}
+                                분야입니다.
+                            </div>
+                        ) : (
+                            ''
+                        )}
+                    </div>
+                    <br />
+                    {isLoading ? (
+                        <div
+                            style={{
+                                width: '12rem',
+                                display: 'flex',
+                                justifyContent: 'center',
+                            }}
+                        >
+                            <LoadingSpinner />
+                        </div>
+                    ) : (
+                        <button onClick={classifyArticle} className={styles.askModel}>
+                            {firstTry ? '분류하기' : '다시하기'}
+                        </button>
+                    )}
+                </div>
+            </div>
+        </div>
     );
-  };
-
-  return (
-    <div className={styles.newsClassifier}>
-      <PageHeader>어떤 기사를 분류하시겠습니까?</PageHeader>
-      <div className={styles.newsClassifierBody}>
-        <textarea
-          ref={articleInputRef}
-          name='article-input'
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-          placeholder={isFocused ? '' : '기사를 입력해주세요.'}
-          className={styles.articleInput}
-          cols='100'
-          rows='48'
-        />
-        <div className={styles.classifyingButtonContainer}>{resultOrGetResult()}</div>
-      </div>
-    </div>
-  );
 }
 
 export default NewsClassifier;
