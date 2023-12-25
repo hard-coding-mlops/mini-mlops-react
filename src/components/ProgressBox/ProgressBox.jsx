5ef2392ae5e2bc8b6ef09a0ec0252f9626dcdac5
taehwan01 <monte6198@gmail.com>
2023-12-26 03:19:37 +0900

    feat: 진행률 시각화 수정

--- src/components/ProgressBox/ProgressBox.jsx
+++ src/components/ProgressBox/ProgressBox.jsx
@@ -15,6 +15,7 @@ function ProgressBox() {
 
     const [closed, setClosed] = useState(true);
     const [scrapeState, setScrapeState] = useState('데이터 수집 중');
+    const [learnState, setLearnState] = useState('모델 학습 중');
 
     useEffect(() => {
         if (scrapeProgress < 51) {
@@ -23,12 +24,27 @@ function ProgressBox() {
             setScrapeState('데이터 정제 중...');
         } else if (scrapeProgress === 100) {
             setScrapeState('데이터 수집 완료');
-            // 3초 후에 scrapeProgress를 0으로 초기화
             setTimeout(() => {
                 dispatch(setScrapeProgress(0));
             }, 5000);
         }
     }, [scrapeProgress]);
+    useEffect(() => {
+        if (learnProgress < 40) {
+            setLearnState('학습 데이터 로드 중...');
+        } else if (learnProgress < 80) {
+            setLearnState('모델 학습 중...');
+        } else if (learnProgress < 90) {
+            setLearnState('결과 시각화 중...');
+        } else if (learnProgress < 100) {
+            setLearnState('모델 저장 중...');
+        } else if (learnProgress === 100) {
+            setLearnState('학습 완료');
+            setTimeout(() => {
+                dispatch(setLearnProgress(-1));
+            }, 5000);
+        }
+    }, [learnProgress]);
 
     return (
         <div className={`${styles.progressBox} ${closed ? '' : styles.show}`}>
@@ -39,35 +55,44 @@ function ProgressBox() {
                 </button>
             </div>
             <hr style={{ border: 'none', height: '1px', backgroundColor: '#7a7a7a' }} />
-            {scrapeProgress === 0 ? (
+            {scrapeProgress === 0 && learnProgress === -1 ? (
                 <div className={styles.empty}>텅 . . .</div>
             ) : (
-                <div>
-                    <span style={{ fontSize: '18px' }}>
-                        - {scrapeState} &nbsp;
-                        <SlotCounter value={scrapeProgress.toFixed(0)} duration={1} dummyCharacterCount={7} />%
-                    </span>
-                    <div className={styles.loadingBar}>
-                        <div
-                            className={styles.currentProgress}
-                            style={{ width: `${scrapeProgress.toFixed(1)}%` }}
-                        ></div>
-                    </div>
-                </div>
-            )}
-            <br />
-            {learnProgress === -1 ? (
-                <div className={styles.empty}>텅 . . .</div>
-            ) : (
-                <div>
-                    <span style={{ fontSize: '18px' }}>
-                        - 모델 학습 중... &nbsp;
-                        <SlotCounter value={learnProgress} duration={1} dummyCharacterCount={7} />%
-                    </span>
-                    <div className={styles.loadingBar}>
-                        <div className={styles.currentProgress} style={{ width: `${learnProgress}%` }}></div>
-                    </div>
-                </div>
+                <>
+                    {scrapeProgress !== 0 && (
+                        <>
+                            <div>
+                                <span style={{ fontSize: '18px' }}>
+                                    - {scrapeState} &nbsp;
+                                    <SlotCounter
+                                        value={scrapeProgress.toFixed(0)}
+                                        duration={1}
+                                        dummyCharacterCount={7}
+                                    />
+                                    %
+                                </span>
+                                <div className={styles.loadingBar}>
+                                    <div
+                                        className={styles.currentProgress}
+                                        style={{ width: `${scrapeProgress.toFixed(1)}%` }}
+                                    ></div>
+                                </div>
+                            </div>
+                            <br />
+                        </>
+                    )}
+                    {learnProgress !== -1 && (
+                        <div>
+                            <span style={{ fontSize: '18px' }}>
+                                - {learnState} &nbsp;
+                                <SlotCounter value={learnProgress} duration={1} dummyCharacterCount={7} />%
+                            </span>
+                            <div className={styles.loadingBar}>
+                                <div className={styles.currentProgress} style={{ width: `${learnProgress}%` }}></div>
+                            </div>
+                        </div>
+                    )}
+                </>
             )}
         </div>
     );
